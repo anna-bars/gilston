cbe40abf901cd88a9f771573975d44324d8a6df4
Anna Bars <abarseghyan863@gmail.com>
2025-11-27 01:49:25 +0400

    maked BottomHeader.tsx adaptive

--- src/components/Layout/Header/BottomHeader.tsx
+++ src/components/Layout/Header/BottomHeader.tsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 import MegaMenu from "./MegaMenu";
 import {navItems} from '../../../data/menudata/menuData'
 import { styles } from "./style/headerstyle";
+import searchIcon from '../../../assets/loupe.png';
+
+import cartIcon from '../../../assets/toolbar/grocery-store.png';
+import userIcon from '../../../assets/toolbar/user.png';
 
 export const NavItem = ({ item, isLast, menuData }: { item: any, isLast: boolean, menuData?: any }) => {
     const [hover, setHover] = useState(false);
@@ -34,26 +38,125 @@ export const NavItem = ({ item, isLast, menuData }: { item: any, isLast: boolean
 
 const BottomHeader = () => { 
     return (
-        <div style={{ backgroundColor: styles.colors.textBlue, boxShadow: '0 3px 8px rgba(115,116,118,1)' }}>
+        <div style={{ 
+            backgroundColor: styles.colors.textBlue, 
+            boxShadow: '0 3px 8px rgba(115,116,118,1)',
+            padding: '10px 0'
+        }}>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-12 d-none d-lg-block">
-                        <ul className="d-flex justify-content-between m-0 p-0 list-unstyled w-100">
-                            {navItems.map((item, index) => (
-                                <NavItem 
-                                    key={item.name} 
-                                    item={item} 
-                                    isLast={index === navItems.length - 1} 
-                                    menuData={item.data} 
-                                />
-                            ))}
-                        </ul>
+                    <div className="col-md-12">
+                        {/* Desktop Menu */}
+                        <div className="d-none d-lg-block">
+                            <ul className="d-flex justify-content-between m-0 p-0 list-unstyled w-100">
+                                {navItems.map((item, index) => (
+                                    <NavItem 
+                                        key={item.name} 
+                                        item={item} 
+                                        isLast={index === navItems.length - 1} 
+                                        menuData={item.data} 
+                                    />
+                                ))}
+                            </ul>
+                        </div>
+
+                        {/* Mobile Menu */}
+                        <div className="d-lg-none">
+                            <div className="row align-items-center">
+                                {/* Search Form */}
+                                <div className="col-7">
+                                    <form action="#" method="post">
+                                        <div className="form-group d-flex align-items-center mb-0">
+                                            <input 
+                                                id="mobsearch"
+                                                type="text" 
+                                                name="search" 
+                                                className="form-control search-input" 
+                                                placeholder="Search"
+                                                style={{
+                                                    height: '35px',
+                                                    border: `1px solid ${styles.colors.lightBlue}`,
+                                                    borderRight: 'none',
+                                                    borderRadius: '4px 0 0 4px',
+                                                    fontSize: '14px'
+                                                }}
+                                            />
+                                            <button 
+                                                type="submit" 
+                                                className="d-flex align-items-center justify-content-center"
+                                                style={{
+                                                    height: '35px',
+                                                    width: '40px',
+                                                    border: `1px solid ${styles.colors.lightBlue}`,
+                                                    borderLeft: 'none',
+                                                    backgroundColor: '#fff',
+                                                    borderRadius: '0 4px 4px 0',
+                                                    cursor: 'pointer'
+                                                }}
+                                            >
+                                                <img src={searchIcon} alt="search" width="16px" />
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+
+                                {/* Account & Cart */}
+                                <div className="col-5">
+                                    <ul className="d-flex justify-content-end list-unstyled m-0">
+                                        <li className="me-3">
+                                            <div className="account-tools text-center">
+                                                <a 
+                                                    href="https://www.globalgilson.com/customer/info" 
+                                                    className="text-decoration-none"
+                                                >
+                                                    <i 
+                                                        className="fa fa-user fs-5" 
+                                                        style={{color: '#fff'}}
+                                                    ><img loading="lazy" src={userIcon} alt="Live Chat" /></i>
+                                                    <p className="m-0 small text-white" style={{fontSize: '12px'}}>Account</p>
+                                                </a>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div className="cart-area text-center position-relative">
+                                                <a 
+                                                    href="https://www.globalgilson.com/cart" 
+                                                    className="text-decoration-none position-relative"
+                                                >
+                                                    <i 
+                                                        className="fa fa-shopping-cart fs-5" 
+                                                        style={{color: '#fff'}}
+                                                    ><img loading="lazy" src={cartIcon} alt="Live Chat" /></i>
+                                                    <span 
+                                                        className="position-absolute badge fw-bold"
+                                                        style={{
+                                                            top: '-5px',
+                                                            right: '-5px',
+                                                            fontSize: '12px',
+                                                            minWidth: '18px',
+                                                            height: '12px',
+                                                            display: 'flex',
+                                                            alignItems: 'center',
+                                                            justifyContent: 'center',
+                                                            padding: '2px 4px',
+                                                            backgroundColor: styles.colors.textBlue,
+                                                            color: '#fff',
+                                                            lineHeight: 1
+                                                        }}
+                                                    >2</span>
+                                                    <p className="m-0 small text-white" style={{fontSize: '12px'}}>Cart</p>
+                                                </a>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     );
 };
-
 
 export default BottomHeader;
