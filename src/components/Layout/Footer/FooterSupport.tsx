68a16b6f8ea5e2256dbc6fa1d1d3c0645b1d6300
Anna Bars <abarseghyan863@gmail.com>
2025-11-30 18:26:47 +0400

    fixed the css issues of FooterSupport.tsx

--- src/components/Layout/Footer/FooterSupport.tsx
+++ src/components/Layout/Footer/FooterSupport.tsx
@@ -60,11 +60,11 @@ const FooterSupport: React.FC = () => {
 
               {/* Desktop version - right side 50% width */}
               <div className="position-absolute top-0 end-0 h-100 w-50 p-4 d-none d-lg-flex flex-column justify-content-center">
-                <h3 className="text-center text-primary fw-bold fs-5 mb-3 text-uppercase">
+                <h3 className="text-center custom-text-primary fw-bold fs-5 mb-4 text-uppercase">
                   We're Dedicated to our customers!
                 </h3>
                 
-                <div className="d-flex justify-content-around align-items-center mb-3">
+                <div className="d-flex justify-content-around align-items-center mb-4">
                   {supportItems.map((item) => (
                     <div key={item.id} className="position-relative text-center">
                       <a 
@@ -76,7 +76,7 @@ const FooterSupport: React.FC = () => {
                         <span className="visually-hidden">{item.actionType}</span>
                       </a>
                       <div className="d-flex flex-column align-items-center">
-                        <div className="bg-warning rounded-1 p-3 mb-2">
+                        <div className="custom-bg-warning rounded-1 p-3 mb-2">
                           <img 
                             src={item.url} 
                             alt={item.label}
@@ -105,7 +105,7 @@ const FooterSupport: React.FC = () => {
               {/* Mobile version - centered at top */}
               <div className="position-absolute top-0 start-0 w-100 p-3 d-lg-none">
                 <div className="d-flex flex-column">
-                  <h3 className="text-center text-primary fw-bold fs-5 mb-3 text-uppercase">
+                  <h3 className="text-center custom-text-primary fw-bold fs-5 mb-3 text-uppercase">
                     We're Dedicated to our customers!
                   </h3>
                   
@@ -121,7 +121,7 @@ const FooterSupport: React.FC = () => {
                           <span className="visually-hidden">{item.actionType}</span>
                         </a>
                         <div className="d-flex flex-column align-items-center">
-                          <div className="bg-warning rounded-1 p-2 mb-1">
+                          <div className="custom-bg-warning rounded-1 p-2 mb-1">
                             <img 
                               src={item.url} 
                               alt={item.label}
@@ -141,7 +141,7 @@ const FooterSupport: React.FC = () => {
                   <div className="position-relative d-flex align-items-center justify-content-center w-100">
                     <hr className="flex-grow-1 text-dark m-0 d-none d-sm-block" style={{maxWidth: '40px'}} />
                     <h4 className="text-center text-dark mb-0 mx-2 mx-sm-3 small">
-                      or check out our <a href="#" className="text-primary fw-bold text-decoration-underline">Support Center</a>
+                      or check out our <a href="#" className="custom-text-primary fw-bold text-decoration-underline">Support Center</a>
                     </h4>
                     <hr className="flex-grow-1 text-dark m-0 d-none d-sm-block" style={{maxWidth: '40px'}} />
                   </div>
