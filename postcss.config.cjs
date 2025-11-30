3e659bdf29ca299ccc1886c14b80c771bee105ee
Anna Bars <abarseghyan863@gmail.com>
2025-11-30 22:55:11 +0400

    fixed the issue -failed to load PostCSS config

---
+++ postcss.config.cjs
@@ -0,0 +1,6 @@
+// postcss.config.cjs
+const IN_PRODUCTION = process.env.NODE_ENV === "production";
+
+module.exports = {
+    autoprefixer: {}
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
@@ -43,94 +44,6 @@ export default defineConfig({
     }
   },
   css: {
-    devSourcemap: false,
-    postcss: {
-      plugins: [
-        require('@fullhuman/postcss-purgecss')({
-          content: [
-            './index.html',
-            './src/**/*.html',
-            './src/**/*.tsx',
-            './src/**/*.ts',
-            './src/**/*.jsx',
-            './src/**/*.js',
-            './src/**/*.css'
-          ],
-          defaultExtractor: (content: string) => content.match(/[\w-/:]+(?<!:)/g) || [],
-          safelist: [
-            /^swiper-/,
-            /^col-/,
-            /^row/,
-            /^container/,
-            /^p-/,
-            /^m-/,
-            /^d-/,
-            /^text-/,
-            /^bg-/,
-            /^justify-/,
-            /^align-/,
-            /^flex/,
-            /^btn/,
-            /^form/,
-            /^input/,
-            /^select/,
-            /^table/,
-            /^modal/,
-            /^carousel/,
-            /^nav/,
-            /^navbar/,
-            /^dropdown/,
-            /^show/,
-            /^active/,
-            /^fade/,
-            /^collapse/,
-            /^collapsing/,
-            /^offcanvas/,
-            /^tooltip/,
-            /^popover/,
-            /^alert/,
-            /^progress/,
-            /^badge/,
-            /^card/,
-            /^list-group/,
-            /^spinner/,
-            /^toast/,
-            /^h/,
-            /^display/,
-            /^lead/,
-            /^small/,
-            /^mark/,
-            /^initialism/,
-            /^blockquote/,
-            /^figure/,
-            /^img/,
-            /^float/,
-            /^position/,
-            /^visible/,
-            /^invisible/,
-            /^ratio/,
-            /^fixed/,
-            /^sticky/,
-            /^vr/,
-            /^opacity/,
-            /^shadow/,
-            /^object/,
-            /^overflow/,
-            /^z-/,
-            /^fs-/,
-            /^fw-/,
-            /^lh-/,
-            /^font/,
-            /^text-opacity/,
-            /^bg-opacity/,
-            /^user-select/,
-            /^pe-/,
-            /^rounded/,
-            /^transition/,
-            /^custom-/  // Ձեր custom class-ների համար
-          ]
-        })
-      ]
-    }
+    devSourcemap: false
   }
 })
